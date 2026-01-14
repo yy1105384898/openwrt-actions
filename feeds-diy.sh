6d7b16374fc7e80c7d5c1dbf313b6c071af34739
yy1105384898 <253683715+yy1105384898@users.noreply.github.com>
2026-01-14 13:57:55 +0800

    Update feed sources to remove branch specifications

--- feeds-diy.sh
+++ feeds-diy.sh
@@ -6,13 +6,13 @@
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 ####################科学上网##########################
-sed -i '$a src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
-sed -i '$a src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
-sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git;dev' feeds.conf.default
-sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' feeds.conf.default
+sed -i '$a src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages' feeds.conf.default
+sed -i '$a src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki' feeds.conf.default
 
 ####################sirpdboy###########################
-sed -i '$a src-git lucky https://github.com/sirpdboy/luci-app-lucky.git;main' feeds.conf.default
-sed -i '$a src-git poweroffdevice https://github.com/sirpdboy/luci-app-poweroffdevice.git;master' feeds.conf.default
+sed -i '$a src-git lucky https://github.com/sirpdboy/luci-app-lucky' feeds.conf.default
+sed -i '$a src-git poweroffdevice https://github.com/sirpdboy/luci-app-poweroffdevice' feeds.conf.default
 ####################VIKINGYFY#########################
-sed -i '$a src-git wolplus https://github.com/VIKINGYFY/packages.git' feeds.conf.default
+sed -i '$a src-git wolplus https://github.com/VIKINGYFY/packages' feeds.conf.default
